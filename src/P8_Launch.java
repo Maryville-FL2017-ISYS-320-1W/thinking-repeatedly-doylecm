dd47f6f4f67b70ece40aa2ddddc3856019a1f504
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 20:35:04 -0600

    Finished

--- src/P8_Launch.java
+++ src/P8_Launch.java
@@ -4,18 +4,25 @@
   	Date: 
 */
 
-// 1. Your predicted output 
+/* 1. T-minus 5, 4, 3, 2, 1,
+ *    Blast off!
 
-
+*/
 
 public class P8_Launch {
 
 	public static void main(String[] args) {
-
+		System.out.println("T-minus ");
+        for( int i = 5; i >= 1; i-- ) {
+            System.out.print( i + ", " );
+        }
+        System.out.println("Blastoff!");
 	}
 
 }
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+// 3. I was close. I thought that the print statement would print on the 
+//    same line as the println statement. But it didn't. But the second 
+// 	  println statement did print on the same line as the print statement.
