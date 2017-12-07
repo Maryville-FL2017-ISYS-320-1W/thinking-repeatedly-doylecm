d13b8083120283dab329167ce688e18f7a2f84e4
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 22:04:12 -0600

    Finished

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -7,7 +7,17 @@
 public class P12_NumbersLeft {
 
 	public static void main(String[] args) {
-
+		final int TOP_SCALE = 5; // highest output
+		for( int line = 1; line <= TOP_SCALE; line++ ) { // for each of 5 lines
+			for( int spacesIndex = 5; spacesIndex >= TOP_SCALE + line; spacesIndex-- ) {  //print spaces
+				System.out.print(" ");
+		}
+		for( int numbersIndex = 1; numbersIndex <= line; numbersIndex++ ) { //repeat the number on the output line
+			System.out.print( line );
+		}
+		System.out.println();
 	}
+
+}
 
 }
