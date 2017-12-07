f81470dbb8828df07135aa31e8bc3407f38e6ec5
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 21:59:36 -0600

    Finished

--- src/P13_NumbersRight.java
+++ src/P13_NumbersRight.java
@@ -7,7 +7,18 @@
 public class P13_NumbersRight {
 
 	public static void main(String[] args) {
-
+		final int TOP_SCALE = 5;// highest output number
+		for( int line = 1; line <= TOP_SCALE; line++ ) { // for each of 5 lines
+			for( int spacesIndex = 1; spacesIndex <= TOP_SCALE - line; spacesIndex++ ) {  // for each line add spaces
+				System.out.print(" ");
+		}
+		for( int numbersIndex = 1; numbersIndex <= line; numbersIndex++ ) { //for each line print and repeat each number the same times as its quantity
+			System.out.print( line );
+		}
+		System.out.println();// move to a new output line
 	}
 
 }
+
+}
+
