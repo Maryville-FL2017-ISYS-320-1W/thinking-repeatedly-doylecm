d7cde62f1fe75bb876b8069d03e4a9262fe8d944
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 20:26:42 -0600

    Finished

--- src/P5_OddStuff.java
+++ src/P5_OddStuff.java
@@ -4,18 +4,27 @@
   	Date: 
 */
 
-// 1. Your predicted output 
-
-
+/* 1.  2
+	   2
+	   2
+	   2
+*/
 
 public class P5_OddStuff {
 
 	public static void main(String[] args) {
-
-	}
-
+		int number = 4;
+        for( int count = 1; count <= number; count++ ) {
+            System.out.println( number );
+            number = number / 2;
+        }
+    }
 }
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+
+/* 3. I was incorrect and it seems that it first prints the number 4,
+then it divides the number, but I don't understand how it printed the 2
+without a print statement?
+*/
