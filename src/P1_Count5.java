0e182228b646bf8f3f74c6021e506ec9277c35f9
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 22:18:03 -0600

    Finished

--- src/P1_Count5.java
+++ src/P1_Count5.java
@@ -6,8 +6,9 @@
 
 public class P1_Count5 {
     public static void main(String[] args) {
-        for( int i = /* FINISH ME */ ) {
-            System.out.println( /* FINISH ME */);
+        for( int line = 1; line <= 4; line++ ) {	// for each of 4 lines
+        	for ( int multiple = 5; multiple <= 5; multiple++) // using a multiple of 5
+            System.out.println( "5 times " + line + " = " + multiple * line); //print line and multiply multiple times line
         }
     }
 }
