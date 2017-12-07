68c85bd7a69791f357fbee8f1ee6d3438640d4ed
doylecm <32971939+doylecm@users.noreply.github.com>
2017-12-06 20:14:34 -0600

    Finished

--- src/P2_HowManyLines.java
+++ src/P2_HowManyLines.java
@@ -4,18 +4,25 @@
   	Date: 
 */
 
-// 1. Your predicted output 
-
-
+/* 1. How many lines
+	  are printed?
+	  How many lines
+	  are printed?
+	  How many lines
+	  are printed?
+*/
 
 public class P2_HowManyLines {
 
 	public static void main(String[] args) {
-
+		for( int i = 1; i <= 3; i++ ) 
+		    System.out.println( "How many lines" );
+		    System.out.println( "are printed?" );
 	}
 
 }
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+// 3. I was not correct. I thought it would print each line three times,
+// but apparently it only applies to the first print statement.
